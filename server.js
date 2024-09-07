4aa7b577eebf0ea772a21470b97cf5e071bb0f58
Zuhair Ahmed <78954298+ZuhairAhmed0@users.noreply.github.com>
2024-09-07 07:35:59 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 const app = require("./app");
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 dotenv.config({ path: "./.env" });
 
 mongoose.connect(process.env.DB_URL_ATLAS, (err) => {
